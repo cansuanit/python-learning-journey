13a6a22b4d42afc6aa0efdc8fd358b91186a119d
coders Club <lukacoderclub@gmail.com>
2026-08-14 17:28:22 +0300

    format.py file is created.

---
+++ 01-objects-and-data-structures/format.py
@@ -0,0 +1,13 @@
+name = "Alice"
+surname = "Smith"
+age = 30
+print("My name is {} {}".format(name, surname))
+print("My name is {0} {1}".format(name, surname))
+print("My name is {1} {0}".format(name, surname))
+print("My name is {s} {n}".format(n=name, s=surname))
+print("My name is {} {} and I am {} years old.".format(name, surname, age))
+
+result = 200/ 500
+print ("the ressult is {r: 1.3}" .fotmat(r = result))
+
+primt(f"the result is {r: 1.3}" .format(r = result))
