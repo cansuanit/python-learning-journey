38cf9e1b1b2861ef499a50f4c0606cd3cb2e004f
cansuanit <cansuakdaganit87@icloud.com>
2026-08-14 13:43:02 +0300

    type-conversion.py file is created.

---
+++ 01-objects-and-data-structures/type-conversion.py
@@ -0,0 +1,54 @@
+"""
+x = input("1. number:")
+y = input("2. number:")
+print("Type of x:", type(x))
+print("Type of y:", type(y))
+sum = int(x) + int(y)
+print("Type of x:", type(x))
+print("Type of y:", type(y))
+print("Sum:", sum)
+print("Type of sum:", type(sum))
+
+"""
+
+x=5 #integer variable   
+y=2.5 #float variable
+name="Cansu" #string variable
+isOnline=True #boolean variable
+
+#print("Type of x:", type(x))
+#print("Type of y:", type(y))        
+#print("Type of name:", type(name))
+#print("Type of isOnline:", type(isOnline))
+
+#Type Conversion
+
+#int to float
+
+"""x=float(x)
+print(x)
+print("Type of x:", type(x))"""
+
+#float to int
+"""y = int(y)
+print(y)    
+print("Type of y:", type(y))"""
+
+#to string
+#x=str(x)
+#y = str(y)
+#print(y)    
+#print("Type of y:", type(y))
+
+
+#result = x + y
+#print("Result:", result)    
+#print("Type of result:", type(result))
+
+#bool to string
+#isOnline = str(isOnline)
+#print("Type of isOnline:", type(isOnline))
+
+#bool to int
+isOnline = int(isOnline)
+print("Type of isOnline:",isOnline, type(isOnline))  
