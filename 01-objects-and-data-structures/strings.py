991f95fb6594ada06049ad08e63652cd03f46593
cansuanit <cansuakdaganit87@icloud.com>
2026-08-14 17:13:42 +0300

    strings.py file is created.

---
+++ 01-objects-and-data-structures/strings.py
@@ -0,0 +1,16 @@
+name = "Cansu"
+surname = "Anıt"
+age = 39
+
+greeting = "My name is " + name + " " + surname + " and \n I am " + str(age) + " years old."
+
+print (greeting)
+print(greeting[0])
+#print(len(greeting))
+#print(greeting[len(greeting)-1])
+#print(greeting[-1])
+#print(greeting[3:7])
+#print(greeting[3:])
+#print(greeting[:7])
+print (greeting[3:7:2])
+
