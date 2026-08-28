1d1b7620954f14dd6663660e4c6d874a184791a3
cansuanit <cansuakdaganit87@icloud.com>
2026-08-28 17:47:56 +0300

    list.py file is added.

---
+++ 01-objects-and-data-structures/list.py
@@ -0,0 +1,26 @@
+message= "Hello There. My name is Cansu Anit".split()
+#message= "Hello There. My name is Cansu Anit"
+#print(message)
+
+# my_list = [1,2,3]
+# my_list = ["bir",2,True,5.6]
+# print(my_list)
+
+list1= ["one","two","three"]
+list2=["four","five","six"]
+numbers = list1 + list2
+print(numbers)
+print (len(numbers))
+print(message[0])
+
+userA= ["Cansu",39]
+userB=["Mira",9]
+users=[userA , userB]
+print(userA)
+print(userB)
+print(users)
+a= users[1]
+
+print(a[0])
+
+print(users[0][0])
