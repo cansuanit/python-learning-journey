ec16af3a9459bff9c86878b47c76d03555acc44b
cansuanit <cansuakdaganit87@icloud.com>
2026-09-07 15:44:55 +0300

    comparisan.py file is added.

---
+++ 02-operators/comparisan.py
@@ -0,0 +1,25 @@
+# username, password => database
+
+# 'cansuanit' , '123456'
+
+a,b,c,d = 5,5,10,4
+password= "1234"
+username="cansuanit"
+result = (a == b) # true
+result = (a==c)
+result = ("cnsnt"==username)
+result= ("cansuanit"== username)
+
+result = (a!=b) #not equal !=
+result = (a>c)
+
+result = (a<c)
+
+result = (a >= b)
+result = (c <= b)
+
+result = (True==1)
+result = (False == 0)
+
+result = True + False +40 
+print(result)
