c1b15c7542f4cb04d0265cef10eaf45c7abc485d
cansuanit <cansuakdaganit87@icloud.com>
2026-09-08 16:23:49 +0300

    if_elif.py file is added.

---
+++ 03-conditional-statements/if_elif.py
@@ -0,0 +1,18 @@
+x = int(input("x :"))
+y= int (input("y :"))
+
+if x > y :
+    print("x yden büyük")
+elif x == y:
+ print ("x ve y bir birine eşit")
+else:
+    print("y x ten büyük")
+
+    num = int(input("sayı : "))
+
+    if num >0 :
+       print("sayı pozitif")
+    elif num == 0:
+       print("sayı 0 dır")
+    else:
+       print("sayı negatif")
